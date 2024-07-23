cce827aff079450873b096d0e3e1c8e5d043e1a0
pitvn2022 <pitvn2022@gmail.com>
2024-07-23 12:20:08 +0700

    user.discriminator - user.id

--- cogs_external/stats.py
+++ cogs_external/stats.py
@@ -51,7 +51,7 @@ class Stats(commands.Cog):
     @is_owner()
     async def slash_stats(self, interaction: discord.Interaction):
         # Log the command usage with user ID
-        LOG.Cmd(f"COMMAND】@{interaction.user.name}#{interaction.user.discriminator} used /stats：")
+        LOG.Cmd(f"COMMAND】@{interaction.user.name}#{interaction.user.id} used /stats：")
 
         # Get bot information
         bot_name = f"{self.bot.user.name}#{self.bot.user.discriminator}"
